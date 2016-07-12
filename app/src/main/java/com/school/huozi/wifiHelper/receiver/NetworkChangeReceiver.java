ceef05b97901832aabcdfe7b173cd0376a26e6b4
huozi <huozi@outlook.com>
2016-07-12 23:12:17 +0800

    Rewrite WIFI portal authentication process.

--- app/src/main/java/com/school/huozi/wifiHelper/receiver/NetworkChangeReceiver.java
+++ app/src/main/java/com/school/huozi/wifiHelper/receiver/NetworkChangeReceiver.java
@@ -9,11 +9,11 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.school.huozi.wifiHelper.utils.HttpUtil;
 
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,45 +25,17 @@ public class NetworkChangeReceiver extends BroadcastReceiver{
     private static final int CONN_CODE = 1;
     private static final int SIGN_CODE = 2;
 
-    private Context mContext;
-
-
-    private Handler handler = new Handler() {
-
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case CONN_CODE:
-                    boolean state = msg.getData().getBoolean("state");
-                    if (state) {
-                        Toast.makeText(mContext, "Network is available.",
-                                Toast.LENGTH_SHORT).show();
-                    } else {
-                        signIn();
-                    }
-                    break;
-                case SIGN_CODE:
-                    boolean signState = msg.getData().getBoolean("state");
-                    String tips = signState ? "available." : "unavailable.";
-                    Toast.makeText(mContext, "WIFI is " + tips,
-                            Toast.LENGTH_SHORT).show();
-            }
-        }
-    };
+    private static Context mContext;
+    private final MHandler mHandler = new MHandler(NetworkChangeReceiver.this);
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
         mContext = context;
 
-        // 读取登记账号
-        SharedPreferences pref = mContext.getSharedPreferences("login_info", Context.MODE_PRIVATE);
-        boolean isLogin = pref.getBoolean("login_flg", false);
-
-        if (!isLogin) {
-            return;
+        if (!readAccount()) {
+            return; // 用户未提前登记wifi账号和密码
         }
-        PARAMS_MAP.put("username", pref.getString("account", ""));
-        PARAMS_MAP.put("password", pref.getString("password", ""));
 
         // 网络连接管理器
         ConnectivityManager connectivityManager = (ConnectivityManager)
@@ -76,18 +48,7 @@ public class NetworkChangeReceiver extends BroadcastReceiver{
             if (!"wifi".equalsIgnoreCase(type)) {
                 return;
             }
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    boolean state = HttpUtil.checkConnect();
-                    Message msg = new Message();
-                    msg.what = CONN_CODE;
-                    Bundle bundle = new Bundle();
-                    bundle.putBoolean("state", state);
-                    msg.setData(bundle);
-                    handler.sendMessage(msg);
-                }
-            }).start();
+            checkConnect(); // 开启新线程测试是否已验证portal.
         } else {
             Toast.makeText(context, "Network is unavailable.",
                     Toast.LENGTH_SHORT).show();
@@ -113,6 +74,26 @@ public class NetworkChangeReceiver extends BroadcastReceiver{
 
 
     /**
+     * 读取用户登记的wifi账号和密码，并设置portal验证的请求参数
+     * @return 如果用户没有提前登记账号和密码，返回false
+     */
+    private boolean readAccount() {
+        // 读取登记账号
+        SharedPreferences pref = mContext.getSharedPreferences("login_info",
+                Context.MODE_PRIVATE);
+        boolean isLogin = pref.getBoolean("login_flg", false);
+
+        if (!isLogin) {
+            return false;
+        }
+        PARAMS_MAP.put("username", pref.getString("account", ""));
+        PARAMS_MAP.put("password", pref.getString("password", ""));
+
+        return true;
+    }
+
+
+    /**
      * 登入校园网账号
      */
     private void signIn() {
@@ -123,13 +104,64 @@ public class NetworkChangeReceiver extends BroadcastReceiver{
             public void run() {
                 String resp = HttpUtil.sendPostRequest(signUrl,
                         new HashMap<String, String>(), PARAMS_MAP);
-                Log.d("", resp);
                 Message msg = new Message();
                 msg.what = SIGN_CODE;
                 Bundle bundle = new Bundle();
                 bundle.putBoolean("state", "login_ok".equalsIgnoreCase(resp));
                 msg.setData(bundle);
-                handler.sendMessage(msg);
+                mHandler.sendMessage(msg);
             }
         }).start();
-    }}
+    }
+
+
+    /**
+     * 开启新线程测试是否已验证portal
+     */
+    private void checkConnect() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean state = HttpUtil.checkConnect();
+                Message msg = new Message();
+                msg.what = CONN_CODE;
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("state", state);
+                msg.setData(bundle);
+                mHandler.sendMessage(msg);
+            }
+        }).start();
+    }
+
+
+    // 静态Handler类，防止内存泄漏
+    private static class MHandler extends  Handler {
+        private WeakReference<NetworkChangeReceiver> mReceiver;
+
+        public MHandler(NetworkChangeReceiver receiver) {
+            mReceiver = new WeakReference<>(receiver);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            NetworkChangeReceiver NcReceiver = mReceiver.get();
+            switch (msg.what) {
+                case CONN_CODE:
+                    boolean state = msg.getData().getBoolean("state");
+                    if (state) {
+                        Toast.makeText(NetworkChangeReceiver.mContext,
+                                "Network is available.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // 进行portal验证
+                        NcReceiver.signIn();
+                    }
+                    break;
+                case SIGN_CODE:
+                    boolean signState = msg.getData().getBoolean("state");
+                    String tips = signState ? "available." : "unavailable.";
+                    Toast.makeText(NetworkChangeReceiver.mContext,
+                            "WIFI is " + tips, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
